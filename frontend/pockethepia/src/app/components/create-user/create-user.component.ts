0c5bdd8c9d2f87a0517c3ea8e2445f50acf83243
Maxime Lovino <maximelovino@gmail.com>
2018-05-13 14:28:24 +0200

    User creation visually done, now backend

--- frontend/pockethepia/src/app/components/create-user/create-user.component.ts
+++ frontend/pockethepia/src/app/components/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-create-user',
@@ -11,7 +12,7 @@ export class CreateUserComponent implements OnInit {
   permissionsFormGroup: FormGroup;
   public hide = true;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, public snackBar: MatSnackBar) {
     this.personalFormGroup = this.fb.group({
       fullName: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
@@ -49,18 +50,34 @@ export class CreateUserComponent implements OnInit {
     return perms;
   }
 
+  public copyLoginInfo() {
+    console.log('Copying login info');
+    const email = this.personalFormGroup.get('email').value;
+    const password = this.personalFormGroup.get('password').value;
+    const infos = { username: email, password };
+    this.clip(JSON.stringify(infos));
+  }
+
   private clip(text: string) {
     document.addEventListener('copy', (e: ClipboardEvent) => {
       e.clipboardData.setData('text/plain', text);
       e.preventDefault();
       document.removeEventListener('copy', null);
     });
+    let snackbarText: string;
     if (document.execCommand('copy')) {
       // TODO do a snackbar or something
+      snackbarText = 'Copied to clipboard';
       console.log('Copy worked');
     } else {
+      snackbarText = 'There was a problem with the copy';
       console.warn('Copy didn\'t work');
     }
+    this.snackBar.open(snackbarText, null, { duration: 2000 });
+  }
+
+  public createUser() {
+
   }
 
   ngOnInit() {
