f32e8beebd5cbc7ecf5fb3005cfbb9e93fe65077
Maxime Lovino <maximelovino@gmail.com>
2018-06-15 17:37:34 +0200

    Update backend package, progress on layouts

--- android/PocketHepia/app/src/main/java/ch/maximelovino/pockethepia/PaymentActivity.kt
+++ android/PocketHepia/app/src/main/java/ch/maximelovino/pockethepia/PaymentActivity.kt
@@ -1,7 +1,11 @@
 package ch.maximelovino.pockethepia
 
+import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import ch.maximelovino.pockethepia.data.AppDatabase
+import kotlinx.android.synthetic.main.activity_payment.*
 
 class PaymentActivity : AppCompatActivity() {
 
@@ -9,6 +13,36 @@ class PaymentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val userID = PreferenceManager.retrieveUserID(this) ?: return
+        val token = PreferenceManager.retrieveToken(this) ?: return
+
+        val db = AppDatabase.getInstance(this)
+
+        val users = db.userDao()
+
+        val currentUser = users.findById(userID)
+
+        currentUser.observe(this, Observer {
+            if (it != null) {
+                if (it.acceptsPayments) {
+                    sending_money_choice_layout.visibility = View.VISIBLE
+                } else {
+                    sending_money_choice_layout.visibility = View.GONE
+                }
+            }
+        })
+
+
+        sending_money_switch.setOnCheckedChangeListener { switch, _ ->
+            sending_money_switch_text.text = if (switch.isChecked) {
+                getString(R.string.sending_money)
+            } else {
+                getString(R.string.receiving_money)
+            }
+        }
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
